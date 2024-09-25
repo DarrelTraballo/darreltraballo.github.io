b7c2a09d0d066076ee792aa53a25f9baccb14670
Darrel Traballo <barreltraballo1121@gmail.com>
2024-09-25 23:43:44 +0800

    my first react component (yippee)

---
+++ src/components/ProjectModal.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react"
+
+function ProjectModal(props) {
+    return (
+        <>
+            <div className="bg-gray-800 shadow-md rounded-lg overflow-hidden flex flex-col">
+                <img src={props.imageSrc} alt={props.title} className="w-full h-auto rounded-lg object-cover" />
+                <div className="p-4 flex flex-col flex-grow">
+                    <h3 className="text-xl font-bold mb-2">{props.title}</h3>
+                    <p className="text-sm text-gray-400 mb-2">
+                        {props.tech.map((tech, index) => (
+                            <span>
+                                {tech}{index !== tech.length - 1 ? ', ' : ''}
+                            </span>
+                        ))}
+                    </p>
+                    <p className="text-gray-400 mb-4">{props.description}</p>
+                    <p className="text-sm text-gray-500 mt-auto">
+                        {props.details.map((detail) => (
+                            <p className="text-sm text-gray-500 mt-auto">{detail}</p>
+                        ))}
+                    </p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ProjectModal
--- src/data.js
+++ src/data.js
@@ -6,7 +6,9 @@ export const data = {
             imageSrc: "/images/Water.gif",
             description: "An attempt to create a simple simulation of waves using Unity's Shader Language, implementing Sum of Sines and Lambertian Diffuse lighting.",
             tech: ["Unity", "C#"],
-            details: "Introductory shader programming project.",
+            details: [
+                "Introductory shader programming project."
+            ],
         },
         {
             id: "2",
@@ -14,7 +16,11 @@ export const data = {
             imageSrc: "/images/Pokemon.gif",
             description: "A simplified recreation of Pokemon's Generation 1 battle system.",
             tech: ["Unity", "C#"],
-            details: "Learned how to use Unity's custom inspector, how to create/edit Scriptable Objects programmatically.",
+            details: [
+                "Used PokeAPI to collect and display Pokemon data.",  
+                "Learned how to use Unity's custom inspector.", 
+                "Learned how to create/edit Scriptable Objects programmatically."
+            ],
         },
         {
             id: "3",
@@ -22,7 +28,9 @@ export const data = {
             imageSrc: "/images/AByteSizedMuseum.png",
             description: "A Game-Based Learning application that introduces computer programming concepts using Wave Function Collapse for procedural generation.",
             tech: ["Unity", "C#"],
-            details: "3D game project developed for my Thesis.",
+            details: [
+                "3D game project developed for my Thesis."
+            ],
         },
         {
             id: "4",
@@ -30,7 +38,9 @@ export const data = {
             imageSrc: "/images/WFC_Python.png",
             description: "A Python-based implementation of the Tile-Based Wave Function Collapse Algorithm",
             tech: ["Python", "Pygame", "Numpy"],
-            details: "Served as a prototype for testing for my final Thesis output.",
+            details: [
+                "Served as a prototype for testing for my final Thesis output."
+            ],
         },
         {
             id: "5",
@@ -38,7 +48,9 @@ export const data = {
             imageSrc: "/images/PCDefender.png",
             description: "A 2D Tower Defense game where you play as a computer's Antivirus Software defending against waves of computer malware.",
             tech: ["Unity", "C#"],
-            details: "Made in 5 days for a University Hackathon.",
+            details: [
+                "Made in 5 days for a University Hackathon."
+            ],
         },
         {
             id: "6",
@@ -46,7 +58,9 @@ export const data = {
             imageSrc: "/images/Scatter.png",
             description: "A 2D RTS game where survival depends on real-time resource scavenging.",
             tech: ["Unity", "C#"],
-            details: "Made in 2 weeks with a friend for the Pirate Software Game Jam 14 (Jan 12, 2024 – Jan 27, 2024).",
+            details: [
+                "Made in 2 weeks with a friend for the Pirate Software Game Jam 14 (Jan 12, 2024 – Jan 27, 2024)."
+            ],
         },
         {
             id: "7",
@@ -54,10 +68,12 @@ export const data = {
             imageSrc: "/images/GeneticAlgorithm.png",
             description: "A Python-based program using a Genetic Algorithm to solve a multi-variable combinatorial optimization problem.",
             tech: ["Python", "Numpy", "Pandas", "Matplotlib"],
-            details: "Made alongside a research manuscript of the same title as a final project for my Discrete Structures 2 course.",
+            details: [
+                "Made alongside a research manuscript of the same title as a final project for my Discrete Structures 2 course."
+            ],
         },
     ],
-    skills: [
+    technologies: [
         {
             // split into two parts: "Preferred Languages" and "Other known Languages"
             // but not yet since there aren't a lot yet xdd
