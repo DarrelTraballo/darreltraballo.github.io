30c97a7f507acc9a02d8a9e9da70313314841b10
Darrel Traballo <barreltraballo1121@gmail.com>
2024-09-27 15:30:16 +0800

    fixed layouting
    layouts should properly transfer properly when making new astro pages now

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,21 +3,21 @@ import { useState } from "react"
 function ProjectModal(props) {
     return (
         <>
-            <div className="bg-gray-800 shadow-md rounded-lg overflow-hidden flex flex-col">
+            <div className="bg-secondary-bg shadow-md rounded-lg overflow-hidden flex flex-col">
                 <img src={props.imageSrc} alt={props.title} className="w-full h-auto rounded-lg object-cover" />
                 <div className="p-4 flex flex-col flex-grow">
                     <h3 className="text-xl font-bold mb-2">{props.title}</h3>
-                    <p className="text-sm text-gray-400 mb-2">
+                    <p className="text-sm text-secondary-font mb-2">
                         {props.tech.map((tech, index) => (
                             <span>
                                 {tech}{index !== tech.length - 1 ? ', ' : ''}
                             </span>
                         ))}
                     </p>
-                    <p className="text-gray-400 mb-4">{props.description}</p>
-                    <p className="text-sm text-gray-500 mt-auto">
+                    <p className="text-secondary-font mb-4">{props.description}</p>
+                    <p className="text-sm text-tertiary-font mt-auto">
                         {props.details.map((detail) => (
-                            <p className="text-sm text-gray-500 mt-auto">{detail}</p>
+                            <p className="text-sm text-tertiary-font mt-auto">{detail}</p>
                         ))}
                     </p>
                 </div>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,18 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'primary-bg': '#111827',
+				'secondary-bg': '#1f2937',
+
+				'primary-font': '#ffffff',
+				'secondary-font': '#9ca3af',
+				'tertiary-font': '#6b7280',
+
+				'link-color': '#60a5fa',
+			},
+		},
 	},
 	plugins: [],
 }
