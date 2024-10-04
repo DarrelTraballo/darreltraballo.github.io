fc153be97a9ede48ae25972494705ce373a51add
Darrel Traballo <barreltraballo1121@gmail.com>
2024-10-05 01:35:01 +0800

    changed profile image, removed profile image shadow

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -1,10 +1,6 @@
 export default function ImageCard({ imageSrc, altText }) {
-    if (imageSrc === "") {
-        imageSrc = "/images/face/profile_pic.png"
-    }
-
     return (
-        <div className="rounded-full shadow-md p-2">
+        <div className="rounded-full p-2">
             <img src={imageSrc} alt={altText} className="w-full h-auto rounded-full object-cover" />
         </div>
     )
